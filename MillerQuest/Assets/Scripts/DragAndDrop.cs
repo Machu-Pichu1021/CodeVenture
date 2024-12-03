72fd289c39b396fddf1d6cfe2ae8129cbd46dc6b
Machu-Pichu1021 <matthewr570@nycstudents.net>
2024-12-03 14:03:24 -0500

    Yeah it don't work

--- MillerQuest/Assets/Scripts/DragAndDrop.cs
+++ MillerQuest/Assets/Scripts/DragAndDrop.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class DragAndDrop : MonoBehaviour
 {
     private Vector3 startPos;
+    [SerializeField] private Vector3 validLocationPos;
 
     private void Start()
     {
         startPos = transform.position;
+        validLocationPos = startPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Valid"))
+            validLocationPos = collision.transform.position;
+        print(validLocationPos);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Valid"))
+            validLocationPos = startPos;
+        print(validLocationPos);
     }
 
     private void OnMouseDrag()
@@ -19,6 +35,6 @@ public class DragAndDrop : MonoBehaviour
 
     private void OnMouseUp()
     {
-        transform.position = startPos;
+        transform.position = validLocationPos;
     }
 }
